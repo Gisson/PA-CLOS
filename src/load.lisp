2813e7c5d21571ba0280ac1265e2967c64dccc1b
touret <miguel_touret@hotmail.com>
2017-04-28 15:26:11 +0100

    not hello world anymore

--- src/load.lisp
+++ src/load.lisp
@@ -1,1 +1,13 @@
-(print 'Hello\ world)
+(defmacro def-class (class &rest values)
+	`(progn (defun ,class (&key ,@values)
+				(vector ,@values)
+			)
+			
+		
+			,@(dolist (val values)
+				`(defun ,(intern(string-upcase (concatenate 'string (string class) "-" (string val) )))()
+						(print 1)
+					)		
+			)	
+			
+		; (def-class lol a) (setf as (LOL :A 1)) (LOL-A as)
