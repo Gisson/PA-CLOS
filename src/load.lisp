4ba50a789025e904aa92952ee93224e052266833
touret <miguel_touret@hotmail.com>
2017-05-04 12:20:58 +0100

    seters done, now time for inheritance

--- src/load.lisp
+++ src/load.lisp
@@ -1,18 +1,20 @@
 (defmacro def-class (class &rest values)
-	`(progn (defun ,(intern(string-upcase (concatenate 'string  "make-" (string class) ))) (&key ,@values)
+	(let ((i -1))
+	`(progn 
+		(defun ,(intern(string-upcase (concatenate 'string  "make-" (string class) ))) (&key ,@values)
 				(vector ,@values)
 			)
 			
-			(defun ,(intern(string-upcase (concatenate 'string  (string class) "?" ))) (class)
-				(and (simple-vector-p class) (equal ,(length (vector values)) (length class) ) )
+		(defun ,(intern(string-upcase (concatenate 'string  (string class) "?" ))) (,class)
+				(and (simple-vector-p class) (equal ,(length (vector values)) (length ,class) ) )
 			)
-			
-		
-			,@(dolist (val values)
-				`(defun ,(intern(string-upcase (concatenate 'string (string class) "-" (string val) )))()
-						(print 1)
-					)		
-			)	
-			
+			,@(mapcar 
+				#'(lambda (x) 
+					(incf i)
+					`(defun ,(intern(string-upcase (concatenate 'string (string class) "-" (string x) ))) (,class)
+						(aref ,class ,i)
+					)
+				) values)
+		)
 	)	
 )
