f4d64a14e737d62e9f035401bf75da5bc4923039
touret <miguel_touret@hotmail.com>
2017-05-11 23:11:34 +0100

    added Inheritance

--- src/load.lisp
+++ src/load.lisp
@@ -1,20 +1,44 @@
 (defmacro def-class (class &rest values)
-	(let ((i 0))
+	(if (not (listp class))
+		(setf class (list class))
+	)
+	
+	(let* ((i 0) 
+	(name (car class))
+	(heir (list name)))
+	
+	(dolist (super (cdr class))
+		(let ((current  (symbol-value (intern(string-upcase (concatenate 'string  "global-" (string super) ))))))
+			(setf heir (append heir current))
+		)
+	)
+	
 	`(progn 
-		(defun ,(intern(string-upcase (concatenate 'string  "make-" (string class) ))) (&key ,@values)
-				(vector (string ,class) ,@values)
+		(setf ,(intern(string-upcase (concatenate 'string  "global-" (string name) ))) ',heir)
+		(defun ,(intern(string-upcase (concatenate 'string  "make-" (string name) ))) (&key ,@values)
+			(let ((classval (vector ',class ,@values)))
+				; (dolist (i ,heir)
+					
+				; )
+			classval
+			)
 		)
 			
-		(defun ,(intern(string-upcase (concatenate 'string  (string class) "?" ))) (,class)
-				(eql (string ,class) (aref ,class 0))
+		(defun ,(intern(string-upcase (concatenate 'string  (string name) "?" ))) (,name)
+			(if (and (simple-vector-p ,name) (listp (aref ,name 0)) (find ,(string name) (mapcar #'string (aref ,name 0)) :test #'equal))
+					t
+				nil
 			)
-			,@(mapcar 
-				#'(lambda (x) 
-					(incf i)
-					`(defun ,(intern(string-upcase (concatenate 'string (string class) "-" (string x) ))) (,class)
-						(aref ,class ,i)
-					)
-				) values)
 		)
-	)	
+			
+			
+		,@(mapcar 
+			#'(lambda (x) 
+					(incf i)
+					`(defun ,(intern(string-upcase (concatenate 'string (string name) "-" (string x) ))) (,name)
+						(aref ,name ,i)
+					)
+			) values
+		)
+	))	
 )
