bd4032b3cffca343a6d1ca6029d2b9447bbcf3d4
Ricardo Mota <ricardo.r.mota@tecnico.ulisboa.pt>
2017-05-15 04:39:51 +0100

    Changed some indentation and added some parameters to a separate line

--- src/load.lisp
+++ src/load.lisp
@@ -6,51 +6,55 @@
         (setf class (list class))
     )
     
-    (let* ((i 0) 
-    (name (car class))
-    (heir (list name)))
+    (let* ((i 0)
+        (name (car class))
+        (heir (list name)))
     
-    (dolist (super (cdr class))
-        (let ((current-heir (symbol-value (gen-symbol "global-" super "-heir" )))
-        (current-values (symbol-value (gen-symbol "global-" super "-values" ))))
-        
-        (setf heir (append heir current-heir))
-            (setf values (append values current-values))
+        (dolist (super (cdr class))
+            (let 
+                ((current-heir (symbol-value (gen-symbol "global-" super "-heir" )))
+                (current-values (symbol-value (gen-symbol "global-" super "-values" ))))
+            
+                (setf heir (append heir current-heir))
+                (setf values (append values current-values))
+            )
+        )
+    
+        `(progn
+            (setf ,(gen-symbol "global-" name "-heir") ',heir)
+            (setf ,(gen-symbol "global-" name "-values") ',values)
+            
+            (defun ,(gen-symbol "make-" name) (&key ,@values)
+                (let* ((table (make-hash-table))
+                    (classval (vector ',class table))
+                    (val-it 0)
+                    (val-names  ',values))
+                    (dolist (val (list ,@values))
+                        (setf (gethash (nth val-it val-names) table) val)
+                        (incf val-it)
+                    )
+                    classval
+                )
+            )
+                
+            (defun ,(gen-symbol name "?") (,name)
+                (if (and
+                        (simple-vector-p ,name)
+                        (listp (aref ,name 0))
+                        (find ,(string name)(mapcar #'string ',heir) :test #'equal))
+                    t
+                    nil
+                )
+            )
+                
+            ,@(mapcar 
+                #'(lambda (x)
+                        (incf i)
+                        `(defun ,(gen-symbol name "-" x) (,name)
+                            (values (gethash ',x (aref ,name 1)))
+                        )
+                ) values
+            )
         )
     )
-    
-    `(progn 
-        (setf ,(gen-symbol "global-" name "-heir") ',heir)
-        (setf ,(gen-symbol "global-" name "-values") ',values)
-        
-        (defun ,(gen-symbol "make-" name) (&key ,@values)
-            (let* ((table (make-hash-table))
-            (classval (vector ',class table))
-            (val-it 0)
-            (val-names  ',values))
-            (dolist (val (list ,@values))
-                (setf (gethash (nth val-it val-names) table) val)
-                (incf val-it)
-            )
-            classval
-            )
-        )
-            
-        (defun ,(gen-symbol name "?") (,name)
-            (if (and (simple-vector-p ,name) (listp (aref ,name 0)) (find ,(string name) (mapcar #'string ',heir) :test #'equal))
-                    t
-                nil
-            )
-        )
-            
-            
-        ,@(mapcar 
-            #'(lambda (x) 
-                    (incf i)
-                    `(defun ,(gen-symbol name "-" x) (,name)
-                        (values (gethash ',x (aref ,name 1)))
-                    )
-            ) values
-        )
-    ))  
 )
